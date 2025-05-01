0d08855d94c0b53bd369d2d71f103303b624f5ba
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 01:00:07 -0700

    Time: 429 ms (53.50%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
 select user_id, max(time_stamp) as last_stamp
 from logins
-where to_char(time_stamp, 'YYYY') = '2020'
+where extract(year from time_stamp) = '2020'
 group by user_id
