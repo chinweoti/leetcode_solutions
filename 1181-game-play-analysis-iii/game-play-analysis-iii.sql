a848196cddc6d382bcea3ee677d580dd290bc2c5
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 22:27:34 -0700

    Time: 644 ms (71.90%) | Memory: 0B (100.00%) - LeetSync

--- 1181-game-play-analysis-iii/game-play-analysis-iii.sql
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select player_id, event_date, sum(games_played) over (partition by player_id order by event_date rows between unbounded preceding and current row) as games_played_so_far
+select player_id, event_date, sum(games_played) over (partition by player_id order by event_date) as games_played_so_far
 from activity
 
 -- when summing all previous rows
