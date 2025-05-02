71766b97cc9982f0ff2112eb157ed0a873174a13
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 23:07:38 -0700

    Time: 297 ms (82.99%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -1,16 +1,16 @@
 -- Write your PostgreSQL query statement below
--- select distinct l1.account_id
--- from loginfo l1 
--- join loginfo l2
--- using(account_id)
--- where l1.ip_address != l2.ip_address
--- and l1.login between l2.login and l2.logout
-
-
-
 select distinct l1.account_id
 from loginfo l1 
-join loginfo l2 
+join loginfo l2
 using(account_id)
-where l1.ip_address != l2.ip_address 
-and ((l1.login, l1.logout) overlaps (l2.login, l2.logout) is true OR l1.logout = l2.login)
+where l1.ip_address != l2.ip_address
+and l1.login between l2.login and l2.logout
+
+
+
+-- select distinct l1.account_id
+-- from loginfo l1 
+-- join loginfo l2 
+-- using(account_id)
+-- where l1.ip_address != l2.ip_address 
+-- and ((l1.login, l1.logout) overlaps (l2.login, l2.logout) is true OR l1.logout = l2.login)
