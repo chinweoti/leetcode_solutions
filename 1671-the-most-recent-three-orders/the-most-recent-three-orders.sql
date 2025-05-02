da8156059338698eaa71b8d69ed613536953874f
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-02 00:27:41 -0700

    Time: 343 ms (80.37%) | Memory: 0B (100.00%) - LeetSync

--- 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
+++ 1671-the-most-recent-three-orders/the-most-recent-three-orders.sql
@@ -1,10 +1,10 @@
 -- Write your PostgreSQL query statement below
 select customer_name, customer_id, order_id, order_date
 from (
-    select name as customer_name, o.customer_id, order_id, order_date, rank() over (partition by o.customer_id order by order_date desc)as rnk
+    select name as customer_name, o.customer_id, order_id, order_date, dense_rank() over (partition by o.customer_id order by order_date desc)as rnk
     from orders o
     join customers c
     on o.customer_id = c.customer_id
     )
-where rnk in (1, 2, 3)
+where rnk <= 3
 order by customer_name asc, customer_id asc, order_date desc
