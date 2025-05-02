49639f9cd19374a2e86f439012af8be86b73f6c4
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 22:00:49 -0700

    Time: 186 ms (79.88%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -3,10 +3,10 @@ select distinct page_id as recommended_page
 from likes
 join friendship
 on user_id in (user1_id, user2_id)
-where 1 in (user1_id, user2_id)
+where user1_id = 1 or user2_id =1
 
-except 
+except
+
 select page_id as recommended_page
 from likes
 where user_id = 1
-
