c5e1b7b7e39c4a85b41beec53af94cc24fcdd80f
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 23:47:31 -0700

    Time: 235 ms (75.75%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,11 +1,8 @@
 -- Write your PostgreSQL query statement below
-with salaries as (
-    select d.name as department, e.name as employee,salary, dense_rank() over (partition by d.name order by salary desc) as rn
-from employee e
-join department d
-on e.departmentId = d.id 
-group by department, employee, salary
-) 
-select department, employee, salary 
-from salaries
+select department, employee,salary--, dense_rank() over (partition by d.name order by salary desc) as rn
+from    (select d.name as department, e.name as employee,salary, dense_rank() over (partition by d.name order by salary desc) as rn
+        from employee e
+        join department d
+        on e.departmentId = d.id 
+        group by department, employee, salary) 
 where rn = 1
