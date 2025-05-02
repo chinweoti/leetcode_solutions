b68a2bbdae95bf2572b4e9c87785622f6d559614
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 18:53:41 -0700

    Time: 190 ms (72.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+select distinct c1.seat_id
+from cinema c1
+join cinema c2
+on abs(c1.seat_id - c2.seat_id) = 1
+where c1.free = 1 and c2.free =1
+order by c1.seat_id
