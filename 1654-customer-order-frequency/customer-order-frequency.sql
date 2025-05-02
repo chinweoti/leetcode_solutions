0884d89f5ff9caad7146138516d8847863e4514f
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 18:04:07 -0700

    Time: 215 ms (81.50%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,26 +1,40 @@
 -- Write your PostgreSQL query statement below
-with june as (
     select c.customer_id, c.name
     from Customers c
     join orders o
     using(customer_id)
     join product p
     using (product_id)
-    where to_char(o.order_date, 'YYYY-MM') = ('2020-06') 
+    where to_char(o.order_date, 'YYYY-MM') IN('2020-06','2020-07') 
     group by c.customer_id, c.name
-    having sum(p.price*o.quantity)>=100
-),
-    july as (
-        select c.customer_id, c.name
-        from Customers c
-        join orders o
-        using(customer_id)
-        join product p
-        using (product_id)
-        where to_char(o.order_date, 'YYYY-MM') = ('2020-07') 
-        group by c.customer_id, c.name
-        having sum(p.price*o.quantity)>=100
-    )
-select customer_id, name from June
-intersect
-select customer_id, name from July
+    having sum(case when extract(month from o.order_date) = 6 then o.quantity * p.price end) >= 100 and
+sum(case when extract(month from o.order_date) = 7 then o.quantity * p.price end) >= 100
+
+
+
+-- using intersect
+-- with june as (
+--     select c.customer_id, c.name
+--     from Customers c
+--     join orders o
+--     using(customer_id)
+--     join product p
+--     using (product_id)
+--     where to_char(o.order_date, 'YYYY-MM') = ('2020-06') 
+--     group by c.customer_id, c.name
+--     having sum(p.price*o.quantity)>=100
+-- ),
+--     july as (
+--         select c.customer_id, c.name
+--         from Customers c
+--         join orders o
+--         using(customer_id)
+--         join product p
+--         using (product_id)
+--         where to_char(o.order_date, 'YYYY-MM') = ('2020-07') 
+--         group by c.customer_id, c.name
+--         having sum(p.price*o.quantity)>=100
+--     )
+-- select customer_id, name from June
+-- intersect
+-- select customer_id, name from July
