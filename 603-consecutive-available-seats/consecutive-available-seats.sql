96e1f66ddab86475a654ce8434ce8d7ecaad103e
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-01 18:55:46 -0700

    Time: 183 ms (83.45%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,7 +1,15 @@
 -- Write your PostgreSQL query statement below
-select distinct c1.seat_id
-from cinema c1
-join cinema c2
-on abs(c1.seat_id - c2.seat_id) = 1
-where c1.free = 1 and c2.free =1
-order by c1.seat_id
+
+-- select distinct c1.seat_id
+-- from cinema c1
+-- join cinema c2
+-- on abs(c1.seat_id - c2.seat_id) = 1
+-- where c1.free = 1 and c2.free =1
+-- order by c1.seat_id
+
+
+
+select distinct seat_id 
+from (select seat_id, free, lag(free,1) over() as lag1_free, lead(free,1) over() as lead1_free from Cinema)
+where (free = 1 and lag1_free = 1)
+or (free = 1 and lead1_free = 1)
