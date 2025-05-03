6b4354942d4e236f0a2ef9e944877cda023b87b0
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-02 23:54:55 -0700

    Time: 195 ms (99.01%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,15 +1,22 @@
 -- Write your PostgreSQL query statement below
-
-select question_id as survey_log 
-from (
-        select question_id, sum(case when answer_id is not null then 1 else 0 end)*1.0/sum(case when action = 'show' then 1 else 0 end) ans
-        from surveylog
-        group by question_id
-        order by ans desc, question_id
-        limit 1 )
+select question_id as survey_log
+from surveylog
+group by question_id
+order by (count(answer_id)::numeric)/count(action) FILTER(where action='show') desc, 1 asc
+limit 1
 
 
+-- another way to solve
+-- select question_id as survey_log 
+-- from (
+--         select question_id, sum(case when answer_id is not null then 1 else 0 end)*1.0/sum(case when action = 'show' then 1 else 0 end) ans
+--         from surveylog
+--         group by question_id
+--         order by ans desc, question_id
+--         limit 1 )
 
+
+-- or this method using cte
 -- with cte as (
 --     select question_id, count(*) filter(where action = 'answer')as ans, count(*) filter(where action = 'show')as shw
 -- from surveylog
