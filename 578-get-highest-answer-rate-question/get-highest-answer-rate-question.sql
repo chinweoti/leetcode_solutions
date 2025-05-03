7648f16777bd19a961bebb1683c08dfab997ccde
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-02 23:53:15 -0700

    Time: 284 ms (30.05%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,28 +1,25 @@
 -- Write your PostgreSQL query statement below
-with cte as (
-    select question_id, count(*) filter(where action = 'answer')as ans, count(*) filter(where action = 'show')as shw
-from surveylog
-group by question_id
-),
-    cte2 as (
-        select question_id, row_number() over (order by ans*100/shw desc, question_id) as rnk
-        from cte
-        where ans > 0
-    )
-select question_id as survey_log
-from  cte2
-where rnk = 1
+
+select question_id as survey_log 
+from (
+        select question_id, sum(case when answer_id is not null then 1 else 0 end)*1.0/sum(case when action = 'show' then 1 else 0 end) ans
+        from surveylog
+        group by question_id
+        order by ans desc, question_id
+        limit 1 )
 
 
--- with cte as(
---     select question_id, count(*) FILTER (WHERE action = 'answer') as answer_cnt,  count(*) FILTER (WHERE action = 'show') as show_cnt
---     from SurveyLog 
---     group by 1 
---     ),
--- cte2 as 
--- (select question_id,row_number() over (order by (answer_cnt*100/show_cnt) desc,question_id) as rn 
--- from cte
--- where answer_cnt > 0)
 
--- select question_id as survey_log from cte2  
--- where rn = 1;
+-- with cte as (
+--     select question_id, count(*) filter(where action = 'answer')as ans, count(*) filter(where action = 'show')as shw
+-- from surveylog
+-- group by question_id
+-- ),
+--     cte2 as (
+--         select question_id, row_number() over (order by ans*100/shw desc, question_id) as rnk
+--         from cte
+--         where ans > 0
+--     )
+-- select question_id as survey_log
+-- from  cte2
+-- where rnk = 1
