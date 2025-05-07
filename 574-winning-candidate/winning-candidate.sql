4061bd5691fab793d61d794c4842d11c6087a9a0
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-07 00:26:10 -0700

    Time: 195 ms (64.22%) | Memory: 0B (100.00%) - LeetSync

--- 574-winning-candidate/winning-candidate.sql
+++ 574-winning-candidate/winning-candidate.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 select name 
 from candidate c
-where id in (select candidateId from vote group by candidateId order by count(candidateId) desc limit 1)
+where id = (select candidateId from vote group by candidateId order by count(candidateId) desc limit 1)
 
 
