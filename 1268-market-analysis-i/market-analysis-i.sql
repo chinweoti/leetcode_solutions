00adc3f012fb9f39d0dfc3ec6d5c2e652230cd73
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-06 23:33:47 -0700

    Time: 521 ms (78.64%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 
-select user_id as buyer_id, u.join_date, count(order_id) filter (where to_char(order_date, 'YYYY') = '2019') as orders_in_2019
+select user_id as buyer_id, u.join_date, coalesce(count(order_id) filter (where to_char(order_date, 'YYYY') = '2019'), 0) as orders_in_2019
 from users u
 left join orders o
 on u.user_id = o.buyer_id
