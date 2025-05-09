06a9a845dd4081654776ca9346f0b70284da504e
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-08 22:21:47 -0700

    Time: 178 ms (68.14%) | Memory: 0B (100.00%) - LeetSync

--- 3530-odd-and-even-transactions/odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -1,7 +1,14 @@
 -- Write your PostgreSQL query statement below
-select transaction_date, coalesce(sum(amount) filter (where amount % 2 = 1),0) as odd_sum,
-                         coalesce(sum(amount) filter (where amount % 2 = 0),0) as even_sum
+-- select transaction_date, coalesce(sum(amount) filter (where amount % 2 = 1),0) as odd_sum,
+--                          coalesce(sum(amount) filter (where amount % 2 = 0),0) as even_sum
 
+-- from transactions
+-- group by transaction_date 
+-- order by transaction_date asc
+
+SELECT transaction_date,
+       SUM(case when amount % 2 != 0 then amount else 0 end) as odd_sum,
+       SUM(case when amount % 2 = 0 then amount else 0 end) as even_sum
 from transactions
-group by transaction_date 
-order by transaction_date asc
+group by 1
+order by 1
