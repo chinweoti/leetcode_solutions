d16bc002f8d2337760978319ae645c03581418c5
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-10 16:09:25 -0700

    Time: 3 ms (40.09%) | Memory: 17.7 MB (88.79%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -1,15 +1,23 @@
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        l = 0 
-        r = 0
-        n = len(nums)
+        # l = 0 
+        # r = 0
+        # n = len(nums)
 
-        while r < n:
-            if abs(l - r) >= indexDifference and abs(nums[l] - nums[r]) >= valueDifference:
-                return[l, r]
-            r += 1
-            if r == n and l < n - 1:
-                l += 1
-                r = l    
-        return [-1, -1]
-        
+        # while r < n:
+        #     if abs(l - r) >= indexDifference and abs(nums[l] - nums[r]) >= valueDifference:
+        #         return[l, r]
+        #     r += 1
+        #     if r == n and l < n - 1:
+        #         l += 1
+        #         r = l    
+        # return [-1, -1]
+
+
+
+
+        for i, vi in enumerate(nums):
+            for j, vj in enumerate(nums):
+                if abs(i - j) >= indexDifference and abs(nums[i] - nums[j]) >= valueDifference:
+                    return [i,j]
+        return [-1,-1]
