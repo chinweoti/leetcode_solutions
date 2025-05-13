dc2afd2831dbb0caf1b979f91a8826decf60e87c
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-12 21:32:03 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (43.87%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,13 +1,26 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        # rows = len(matrix)
+        # cols = len(matrix[0])
+        
+        # # transposed_matrix = [[0]* rows for _ in range(cols)]
+        # transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
+        
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         transposed_matrix[j][i] = matrix[i][j]
+                
+        # return transposed_matrix
+
+
+
         rows = len(matrix)
         cols = len(matrix[0])
-        
-        # transposed_matrix = [[0]* rows for _ in range(cols)]
-        transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
-        
-        for i in range(rows):
-            for j in range(cols):
-                transposed_matrix[j][i] = matrix[i][j]
-                
-        return transposed_matrix
+
+        res = []
+        for i in range(cols):
+            row = []
+            for j in range(rows):
+                row.append(matrix[j][i])
+            res.append(row)
+        return res
