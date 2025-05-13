a178c43a571df6519b5d129022800089dad61bf1
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-12 21:58:47 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (22.96%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,21 +1,21 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        # Sort both the strings
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
+        # # Sort both the strings
+        # sorted_s = sorted(s)
+        # sorted_t = sorted(t)
 
-        # Character by character comparison
-        i = 0
-        while i < len(s):
-            if sorted_s[i] != sorted_t[i]:
-                return sorted_t[i]
-            i += 1
+        # # Character by character comparison
+        # i = 0
+        # while i < len(s):
+        #     if sorted_s[i] != sorted_t[i]:
+        #         return sorted_t[i]
+        #     i += 1
 
-        return sorted_t[i]
+        # return sorted_t[i]
 
 
 
-        # for i in t:
-        #     if i not in s:
-        #         return i
+        for i in t:
+            if t.count(i) > s.count(i):
+                return(i) 
             
