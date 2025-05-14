f32b134bf1fec9d727a1a26a3c00865810c62dae
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-14 15:45:54 -0700

    Time: 42 ms (22.74%) | Memory: 17.7 MB (42.09%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -1,15 +1,15 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         
-        # return address.replace(".","[.]")
+        return address.replace(".","[.]")
 
 
-        res = ""
+        # res = ""
         
-        for e in address:
-            if e != ".":
-                res += e
-            else:
-                res += '[.]'
+        # for e in address:
+        #     if e != ".":
+        #         res += e
+        #     else:
+        #         res += '[.]'
 
-        return res
+        # return res
