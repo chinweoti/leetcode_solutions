c959200c6fd624b7e39bee0f52169b9b87acf463
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-14 00:01:43 -0700

    Time: 14 ms (79.52%) | Memory: 18 MB (34.93%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,44 @@
+class Solution:
+    from collections import defaultdict
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        mMap = Counter(magazine)
+        # print(mMap)
+        rMap = Counter(ransomNote)
+        # print(rMap)
+
+        for char in rMap:
+            if char not in mMap:
+                return False
+            if rMap[char] > mMap[char]:
+                return False
+        
+        return True
+
+
+
+
+
+        # note = defaultdict(int)
+
+        # if len(magazine) < len(ransomNote):
+        #     return False
+
+
+        # for i in range(len(magazine)):
+        #     note[magazine[i]] += 1
+        #     print(note)
+            
+        # for e in ransomNote:    
+        #     if e not in note: 
+        #         print(e)
+        #         return False
+        #     else:
+        #         if note[e]> 0:
+        #             note[e] -= 1
+        #             print(e)
+
+        # return True
+
+        
+
