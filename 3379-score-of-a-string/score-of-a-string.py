04ad8684abeefc00cb765a2ebe537862181935df
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-13 21:23:29 -0700

    Time: 3 ms (26.20%) | Memory: 17.9 MB (28.80%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -1,15 +1,15 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        score = 0
-     
-        for i in range(len(s) - 1):
-            x = (ord(s[i]) - ord('a'))
-            y = (ord(s[i+1]) - ord('a'))
-            score += abs(x- y)
-        return score
-
-
         # score = 0
+     
         # for i in range(len(s) - 1):
-        #     score += abs(ord(s[i]) - ord(s[i + 1]))
+        #     x = (ord(s[i]) - ord('a'))
+        #     y = (ord(s[i+1]) - ord('a'))
+        #     score += abs(x- y)
         # return score
+
+
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+        return score
