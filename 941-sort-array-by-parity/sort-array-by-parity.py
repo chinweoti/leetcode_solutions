795d22e6c6cff74258a9178127b43ccc42e079dc
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-14 23:03:34 -0700

    Time: 3 ms (47.61%) | Memory: 18.6 MB (35.87%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,7 +1,7 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         l = 0
-        nums.sort()
+        # nums.sort()
 
         for i, n in enumerate(nums):
             if nums[i]% 2 == 0:
@@ -10,3 +10,19 @@ class Solution:
                 nums[i] = temp
                 l += 1
         return nums
+        # if you dont want a temp holder, exchange both in one line ie nums[l], nums[i] = nums[i], nums[l]
+        # time complexity = O(nlog n)
+        # space complexity = O(log n)
+
+
+        # l, r = 0, len(nums) - 1
+        # while l < r:
+        #     if nums[l] % 2 == 0:
+        #         l += 1
+        #     elif nums[r] % 2 != 0:
+        #         r -= 1
+        #     else:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         left += 1
+        #         right -= 1
+        # return nums
