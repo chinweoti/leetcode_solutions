b3facac11533ba7f7bc8ac6a506ecc9091ac3d8a
Chinwe Oti <88626580+chinweoti@users.noreply.github.com>
2025-05-18 22:24:04 -0700

    Time: 18 ms (75.97%) | Memory: 18.4 MB (16.29%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        freq = Counter(num for num in nums if num%2 == 0 )
+        
+
+        if not freq:
+            return -1
+
+        max_freq = max(freq.values())
+    
+        m_freq = [k for k, v in freq.items() if v == max_freq]
+        print(m_freq)
+        return min(m_freq)
+        
